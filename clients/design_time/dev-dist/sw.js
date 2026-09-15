99f29252aa31f259e1656b7ab75f074569703d71
leeh <sk851234@naver.com>
2026-09-15 19:04:11 +0900

    Revert "Docker 환경 구성 서버/클라이언트/데이터베이스"
    This reverts commit 6b6012046d4ad2cfe5327a172a3778c3fe78e5b8.

--- clients/design_time/dev-dist/sw.js
+++ clients/design_time/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f1jvpg3cds4"
+    "revision": "0.5tv7ejb2enk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
